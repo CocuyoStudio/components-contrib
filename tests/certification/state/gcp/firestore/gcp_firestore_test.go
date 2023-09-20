92da73aebdf71dee52ce3f3b65e7d9aef80a7efb
robertojrojas <robertojrojas@gmail.com>
2023-09-20 12:08:56 -0400

    adds pubsub test
    Signed-off-by: robertojrojas <robertojrojas@gmail.com>

--- tests/certification/state/gcp/firestore/gcp_firestore_test.go
+++ tests/certification/state/gcp/firestore/gcp_firestore_test.go
@@ -14,23 +14,31 @@ limitations under the License.
 package gcp_firestore_test
 
 import (
+	"context"
 	"fmt"
-	"strconv"
+	"log"
+	"os"
 	"testing"
 
-	firestore "github.com/dapr/components-contrib/state/gcp/firestore"
-	"github.com/dapr/components-contrib/tests/certification/embedded"
-	"github.com/dapr/components-contrib/tests/certification/flow"
-	"github.com/dapr/go-sdk/client"
-
-	secretstore_env "github.com/dapr/components-contrib/secretstores/local/env"
-	"github.com/dapr/components-contrib/tests/certification/flow/sidecar"
-	secretstores_loader "github.com/dapr/dapr/pkg/components/secretstores"
-	state_loader "github.com/dapr/dapr/pkg/components/state"
-	dapr_testing "github.com/dapr/dapr/pkg/testing"
-	"github.com/dapr/kit/logger"
-	"github.com/stretchr/testify/assert"
+	"cloud.google.com/go/pubsub"
 )
+
+// "fmt"
+// "strconv"
+// "testing"
+
+// firestore "github.com/dapr/components-contrib/state/gcp/firestore"
+// "github.com/dapr/components-contrib/tests/certification/embedded"
+// "github.com/dapr/components-contrib/tests/certification/flow"
+// "github.com/dapr/go-sdk/client"
+
+// secretstore_env "github.com/dapr/components-contrib/secretstores/local/env"
+// "github.com/dapr/components-contrib/tests/certification/flow/sidecar"
+// secretstores_loader "github.com/dapr/dapr/pkg/components/secretstores"
+// state_loader "github.com/dapr/dapr/pkg/components/state"
+// dapr_testing "github.com/dapr/dapr/pkg/testing"
+// "github.com/dapr/kit/logger"
+// "github.com/stretchr/testify/assert"
 
 const (
 	sidecarNamePrefix = "firestore-sidecar-"
@@ -38,92 +46,116 @@ const (
 )
 
 func TestGCPFirestoreStorage(t *testing.T) {
-	ports, err := dapr_testing.GetFreePorts(2)
-	assert.NoError(t, err)
 
-	currentGrpcPort := ports[0]
-	currentHTTPPort := ports[1]
+	ctx := context.Background()
 
-	basicTest := func(statestore string) flow.Runnable {
-		return func(ctx flow.Context) error {
-			client, err := client.NewClientWithPort(fmt.Sprint(currentGrpcPort))
-			if err != nil {
-				panic(err)
-			}
-			defer client.Close()
+	// Sets your Google Cloud Platform project ID.
+	projectID := os.Getenv("GCP_PROJECT_ID")
 
-			stateKey := key
-			stateValue := "certificationdata"
+	// Creates a client.
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
 
-			// save state, default options: strong, last-write
-			err = client.SaveState(ctx, statestore, stateKey, []byte(stateValue), nil)
-			assert.NoError(t, err)
+	// Sets the id for the new topic.
+	topicID := os.Getenv("GCP_CERT_TEST_TOPIC")
 
-			// get state
-			item, err := client.GetState(ctx, statestore, stateKey, nil)
-			assert.NoError(t, err)
-			assert.NotNil(t, item)
-			assert.Equal(t, stateValue, string(item.Value))
-
-			// delete state
-			err = client.DeleteState(ctx, statestore, stateKey, nil)
-			assert.NoError(t, err)
-
-			return nil
-		}
+	// Creates the new topic.
+	topic, err := client.CreateTopic(ctx, topicID)
+	if err != nil {
+		log.Fatalf("Failed to create topic: %v", err)
 	}
 
-	flow.New(t, "Test basic operations").
-		// Run the Dapr sidecar with GCP Firestore storage.
-		Step(sidecar.Run(sidecarNamePrefix,
-			append(componentRuntimeOptions(),
-				embedded.WithoutApp(),
-				embedded.WithDaprGRPCPort(strconv.Itoa(currentGrpcPort)),
-				embedded.WithDaprHTTPPort(strconv.Itoa(currentHTTPPort)),
-				embedded.WithComponentsPath("./components/basictest"),
-			)...,
-		)).
-		Step("Run basic test with master key", basicTest("statestore-basic")).
-		Run()
+	fmt.Printf("Topic %v created.\n", topic)
 
-	flow.New(t, "Test entity_kind").
-		Step(sidecar.Run(sidecarNamePrefix,
-			append(componentRuntimeOptions(),
-				embedded.WithoutApp(),
-				embedded.WithDaprGRPCPort(strconv.Itoa(currentGrpcPort)),
-				embedded.WithDaprHTTPPort(strconv.Itoa(currentHTTPPort)),
-				embedded.WithComponentsPath("./components/entity_kind"),
-			)...,
-		)).
-		Step("Run basic test with master key", basicTest("statestore-basic")).
-		Run()
+	// ports, err := dapr_testing.GetFreePorts(2)
+	// assert.NoError(t, err)
 
-	flow.New(t, "Test NoIndex").
-		Step(sidecar.Run(sidecarNamePrefix,
-			append(componentRuntimeOptions(),
-				embedded.WithoutApp(),
-				embedded.WithDaprGRPCPort(strconv.Itoa(currentGrpcPort)),
-				embedded.WithDaprHTTPPort(strconv.Itoa(currentHTTPPort)),
-				embedded.WithComponentsPath("./components/noindex"),
-			)...,
-		)).
-		Step("Run basic test with master key", basicTest("statestore-basic")).
-		Run()
+	// currentGrpcPort := ports[0]
+	// currentHTTPPort := ports[1]
+
+	// basicTest := func(statestore string) flow.Runnable {
+	// 	return func(ctx flow.Context) error {
+	// 		client, err := client.NewClientWithPort(fmt.Sprint(currentGrpcPort))
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		defer client.Close()
+
+	// 		stateKey := key
+	// 		stateValue := "certificationdata"
+
+	// 		// save state, default options: strong, last-write
+	// 		err = client.SaveState(ctx, statestore, stateKey, []byte(stateValue), nil)
+	// 		assert.NoError(t, err)
+
+	// 		// get state
+	// 		item, err := client.GetState(ctx, statestore, stateKey, nil)
+	// 		assert.NoError(t, err)
+	// 		assert.NotNil(t, item)
+	// 		assert.Equal(t, stateValue, string(item.Value))
+
+	// 		// delete state
+	// 		err = client.DeleteState(ctx, statestore, stateKey, nil)
+	// 		assert.NoError(t, err)
+
+	// 		return nil
+	// 	}
+	// }
+
+	// flow.New(t, "Test basic operations").
+	// 	// Run the Dapr sidecar with GCP Firestore storage.
+	// 	Step(sidecar.Run(sidecarNamePrefix,
+	// 		append(componentRuntimeOptions(),
+	// 			embedded.WithoutApp(),
+	// 			embedded.WithDaprGRPCPort(strconv.Itoa(currentGrpcPort)),
+	// 			embedded.WithDaprHTTPPort(strconv.Itoa(currentHTTPPort)),
+	// 			embedded.WithComponentsPath("./components/basictest"),
+	// 		)...,
+	// 	)).
+	// 	Step("Run basic test with master key", basicTest("statestore-basic")).
+	// 	Run()
+
+	// flow.New(t, "Test entity_kind").
+	// 	Step(sidecar.Run(sidecarNamePrefix,
+	// 		append(componentRuntimeOptions(),
+	// 			embedded.WithoutApp(),
+	// 			embedded.WithDaprGRPCPort(strconv.Itoa(currentGrpcPort)),
+	// 			embedded.WithDaprHTTPPort(strconv.Itoa(currentHTTPPort)),
+	// 			embedded.WithComponentsPath("./components/entity_kind"),
+	// 		)...,
+	// 	)).
+	// 	Step("Run basic test with master key", basicTest("statestore-basic")).
+	// 	Run()
+
+	// flow.New(t, "Test NoIndex").
+	// 	Step(sidecar.Run(sidecarNamePrefix,
+	// 		append(componentRuntimeOptions(),
+	// 			embedded.WithoutApp(),
+	// 			embedded.WithDaprGRPCPort(strconv.Itoa(currentGrpcPort)),
+	// 			embedded.WithDaprHTTPPort(strconv.Itoa(currentHTTPPort)),
+	// 			embedded.WithComponentsPath("./components/noindex"),
+	// 		)...,
+	// 	)).
+	// 	Step("Run basic test with master key", basicTest("statestore-basic")).
+	// 	Run()
 }
 
-func componentRuntimeOptions() []embedded.Option {
-	log := logger.NewLogger("dapr.components")
+// func componentRuntimeOptions() []embedded.Option {
+// 	log := logger.NewLogger("dapr.components")
 
-	stateRegistry := state_loader.NewRegistry()
-	stateRegistry.Logger = log
-	stateRegistry.RegisterComponent(firestore.NewFirestoreStateStore, "gcp.firestore")
+// 	stateRegistry := state_loader.NewRegistry()
+// 	stateRegistry.Logger = log
+// 	stateRegistry.RegisterComponent(firestore.NewFirestoreStateStore, "gcp.firestore")
 
-	secretstoreRegistry := secretstores_loader.NewRegistry()
-	secretstoreRegistry.Logger = log
-	secretstoreRegistry.RegisterComponent(secretstore_env.NewEnvSecretStore, "local.env")
+// 	secretstoreRegistry := secretstores_loader.NewRegistry()
+// 	secretstoreRegistry.Logger = log
+// 	secretstoreRegistry.RegisterComponent(secretstore_env.NewEnvSecretStore, "local.env")
 
-	return []embedded.Option{
-		embedded.WithStates(stateRegistry),
-		embedded.WithSecretStores(secretstoreRegistry),
-	}
-}
+// 	return []embedded.Option{
+// 		embedded.WithStates(stateRegistry),
+// 		embedded.WithSecretStores(secretstoreRegistry),
+// 	}
+// }
